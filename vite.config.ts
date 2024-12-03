af82bcd06aff17f90ab4dadf3f0eeeae9ded3a54
djwirz <djwirz@gmail.com>
2024-12-03 10:01:01 -0500

    properly config tailwind using the vite specific packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-import tailwindcss from 'tailwindcss';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
       manifest: {
@@ -27,9 +28,4 @@ export default defineConfig({
       },
     }),
   ],
-  css: {
-    postcss: {
-      plugins: [tailwindcss],
-    },
-  },
 });
